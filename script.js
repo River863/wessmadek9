3a5829b6cf3f1d4441281bc375d1ac0b781b7d54
River863 <79874502+River863@users.noreply.github.com>
2026-07-02 20:12:09 -0700

    Add scroll event listener to change navbar background

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+        nav.style.background = "#111";
+    } else {
+        nav.style.background = "transparent";
+    }
+});
