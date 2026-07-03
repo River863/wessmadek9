818a6a9ea2972d2ec0704cd77f289f91c105ba63
River863 <79874502+River863@users.noreply.github.com>
2026-07-02 23:34:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
-
-    if(window.scrollY > 50){
-        nav.style.background = "#111";
-    } else {
-        nav.style.background = "transparent";
-    }
+// Smooth scroll for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
 });
